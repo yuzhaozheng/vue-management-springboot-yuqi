fc962b5348019a2dc811a866f577671d03e01fa1
yuzhaozheng <2816648788@qq.com>
2020-05-16 17:35:57 +0800

    服务器端代码框架

---
+++ server/commons/src/main/java/vom/yuzhaozheng/management/commons/RequestTool.java
@@ -0,0 +1,7 @@
+package vom.yuzhaozheng.management.commons;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RequestTool {
+}
---
+++ server/management/src/test/java/com/yuzhaozheng/management/ManagementTest.java
@@ -0,0 +1,12 @@
+package com.yuzhaozheng.management;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ManagementTest {
+    @Test
+    public void tests(){
+
+    }
+}
